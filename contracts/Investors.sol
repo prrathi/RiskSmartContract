5cd9a0d73502bed004ae93334d3b8cd2c5430a87
Praneet Rathi <prrathi10@gmail.com>
2022-02-16 20:36:26 -0600

    set organization and began functionality for three contracts

--- contracts/Investors.sol
+++ contracts/Investors.sol
@@ -1,13 +1,57 @@
 pragma solidity >=0.5.17;
+import "./Platforms.sol";
 
-contract Investor {
-    uint tokenonecnt;
-    uint tokentwocnt;
-    uint tokenthreecnt;
-    uint32 totalcapital;
-    uint32 maxloss;
-    function initialize(uint32 capital, uint32 maxloss) public returns (bool);
-    function change(uint32 diff) public returns (bool);
-    function withdraw() public returns (bool);
-    function getProfit() public returns (uint);
+contract InvestorFactory is Platform{
+
+    // put these in platforms.sol?
+    // fillerValues, use extra zeroes as equivalent to decimal points
+    uint token1ratio = 1000;
+    uint token2ratio = 2000;
+    uint token3ratio = 3000;
+    struct Investor {
+        uint token1cnt;
+        uint token2cnt;
+        uint token3cnt;
+        uint maxLoss;
+        uint currLoss;
+        uint value;
+        // each token represents quantity of different ratioed (capital:maxloss) investments
+        // can set temporary constraint that investor can't have multiple types of tokens
+    }
+    uint _totalInvestorRisk;
+    bool finished = false;
+    bool finishedAlready = false;
+
+    mapping (address => uint) investorToId;
+
+    event newInvestor(uint _id, uint _token1cnt, uint _token2cnt, uint _token3cnt);
+
+    Investor[] public investors;
+
+    function createInvestor(uint _token1cnt, uint _token2cnt, uint _token3cnt ) private {
+        require(investorToId[msg.sender] == 0);
+        uint _maxLoss = (_token1cnt + _token2cnt + _token3cnt) * 1000;
+        investors.push(Investor(_token1cnt, _token2cnt, _token3cnt, _maxLoss, 0, 0));
+        uint _id = investors.length;
+        investorToId[msg.sender] = _id;
+        // some indicator or capping factor would set finished to true
+        _totalInvestorRisk += _maxLoss;
+        if (finished && !finishedAlready) {
+            setInvestorRisk(_totalInvestorRisk);
+            finishedAlready = true;
+        }
+        emit newInvestor(_id, _token1cnt, _token2cnt, _token3cnt);
+    }
+    
+    function splitClaim(uint _claim) internal {
+        //dostuff
+    }
+
+    // function change(uint32 diff) public returns (bool);
+    // function withdraw() public returns (bool);
+    function getProfit() public view returns (uint) {
+        require(investorToId[msg.sender] != 0);
+        Investor memory iTemp = investors[investorToId[msg.sender]-1];
+        return iTemp.value - iTemp.currLoss;
+    }
 }
